5bd90c14c86db768f53a24e80041429c56f56657
devambarbhaya <22b2113@iitb.ac.in>
2023-12-15 18:43:35 +0530

    recommended list

---
+++ lib/recommended-service.ts
@@ -0,0 +1,12 @@
+import { db } from "./db";
+import { getSelf } from "./auth-service";
+import { resolve } from "path";
+export const getRecommended = async () => {
+    const users = await db.user.findMany({
+        orderBy: {
+            createdAt: "desc"
+        }
+    })
+
+    return users
+}
